25f570b2b07c961ed6884eb9ec6928bbfb3604fa
pcipov <pcipov@twilio.com>
2021-12-02 19:23:58 +0100

    fixing path

--- build.images.sh
+++ build.images.sh
@@ -5,7 +5,7 @@ apt-get install -y curl
 
 curl -fsSL -o dist.tar.gz  https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.3.0/graalvm-ce-java11-linux-amd64-21.3.0.tar.gz
 tar -xzf dist.tar.gz
-export PATH=/graalvm-ce-java11-21.3.0/bin:$PATH
+export PATH=/build/graalvm-ce-java11-21.3.0/bin:$PATH
 
 gu install native-image
 

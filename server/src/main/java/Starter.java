afecb732aeb34e8ba43a2868f5bf78322a0f581f
pcipov <pcipov@twilio.com>
2021-12-02 13:06:39 +0100

    setting dns cache to be ommited

--- server/src/main/java/Starter.java
+++ server/src/main/java/Starter.java
@@ -2,6 +2,9 @@ import kafka.Kafka;
 
 public class Starter {
     public static void main(String[] args) {
+        java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
+        java.security.Security.setProperty("networkaddress.cache.negative.ttl" , "0");
+
         System.getProperties().list(System.out);
         Kafka.main(args);
     }

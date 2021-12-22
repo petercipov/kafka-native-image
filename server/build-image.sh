16a4ee0bc6f550e6936f41b664022b3378b0b0c7
pcipov <pcipov@twilio.com>
2021-12-22 11:02:54 +0100

    using assited config tracing tool to create build configuration

--- server/build-image.sh
+++ server/build-image.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+set -o xtrace
+
 native-image \
-  -jar ./target/server-1.0.jar --allow-incomplete-classpath \
-  -J--add-exports=java.management/sun.management=ALL-UNNAMED \
-  -H:ReflectionConfigurationFiles=./reflection-config.json \
-  -H:ResourceConfigurationFiles=./resource-config.json \
-  -H:DynamicProxyConfigurationFiles=./proxy-config.json \
-  -H:+StaticExecutableWithDynamicLibC \
+  -jar ./target/server-1.0.jar \
+  -H:ConfigurationFileDirectories=./assisted-config \
+  -H:+ReportExceptionStackTraces \
   -H:+TraceNativeToolUsage \
+  --allow-incomplete-classpath \
   --native-image-info \
   --verbose \
   --no-fallback \
+  -H:+StaticExecutableWithDynamicLibC \
   --static

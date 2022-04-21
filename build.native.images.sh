00cc594b05bf958f9d4f603759892fbf1e7aeafe
pcipov <pcipov@twilio.com>
2022-04-21 11:43:53 +0200

    adding maven step

--- build.native.images.sh
+++ build.native.images.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-
-curl -o  apache-maven-3.8.4-bin.tar.gz https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
-tar -xvf ./apache-maven-3.8.4-bin.tar.gz
-./apache-maven-3.8.4/bin/mvn clean package
-
+mvn clean package
 cd server && sh ./build-image.sh && cd ..
 
 mkdir -p ./kafka
